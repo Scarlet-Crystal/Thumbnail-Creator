7022b7b22e9d847b05b60afe85c42b4c840825a0
Scarlet-Crystal <141389226+Scarlet-Crystal@users.noreply.github.com>
2023-10-24 05:05:27 -0600

    Disable super-sampling by default

--- Packages/com.vrchat.users.scarlet-crystal.thumbnailcreator/Runtime/ThumbnailCreator.cs
+++ Packages/com.vrchat.users.scarlet-crystal.thumbnailcreator/Runtime/ThumbnailCreator.cs
@@ -18,6 +18,6 @@ namespace ThumbnailUtilities
         + "\nHigher levels will use more VRAM."
         + "\nNone will disable supersampling. Use this option if you wish to use MSAA or"
         + " postprocessing-based antialiasing instead.")]
-        public SSAAQuality supersampleLevel = SSAAQuality.Medium;
+        public SSAAQuality supersampleLevel = SSAAQuality.None;
     }
 }
